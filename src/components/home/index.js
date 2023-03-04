db281ade43beb096dfc279e119c388862f7abf48
f1vlad <kulchitski@gmail.com>
2023-03-04 16:03:39 -0500

    Header and footer components import to homepage

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,35 +1,35 @@
 import React from "react";
 import { useState } from "react";
+import styles from "./Home.module.scss";
+import StaticHeader from "../staticHeader";
+import Footer from "../Footer";
 
 const Home = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   return (
     <>
       <div className="news-index">
-        <div>
-          <div>
-            <img src='/images/logo/OISCLogo150.png' />
-            <h3>Open</h3>
-            <p>Index Systems</p>
-            <>
-              initial load
-            </>
+        <StaticHeader />
 
-            <br />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(searchQuery);
+            window.location = `/search?q=${encodeURIComponent(searchQuery)}`;
+          }}
+        >
+          <input
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+          <button type="submit"> Submit</button>
+        </form>
 
-            <form onSubmit={e => {
-              e.preventDefault();
-              console.log(searchQuery);
-              window.location = `/search?q=${encodeURIComponent(searchQuery)}`;
-            }}>
-            <input placeholder="Search" value={searchQuery} onChange={e => {
-              setSearchQuery(e.target.value)
-            }}/>
-            <button type="submit"> Submit</button>
-            </form>
-          </div>
-        </div>
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/staticHeader/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StaticHeader = () => {
+  return (
+    <>
+      HEADER
+    </>
+  );
+};
+
+export default StaticHeader;
