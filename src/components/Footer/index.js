684c5e5c2e005d6731ec6adb05461868b5d7fb28
f1vlad <kulchitski@gmail.com>
2023-03-04 16:39:45 -0500

    Footer design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+import styles from './Footer.module.scss';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      FOOTER
+      <footer className={styles.footer}>
+        <section className={styles.links}>
+          <Link href='#'>About</Link>
+          <Link href='#'>Privacy</Link>
+          <Link href='#'>Transparency</Link>
+          <Link href='#'>Careers</Link>
+          <Link href='#'>Feedback</Link>
+          <Link href='#'>Contact Us</Link>
+        </section>
+        <section className={styles.copyright}>
+          Copyright 2023 Open Index Systems LLC. All Rights Reserved.
+        </section>
+      </footer>
     </>
   );
 };
--- src/components/staticHeader/index.js
+++ src/components/staticHeader/index.js
@@ -1,20 +1,24 @@
 import React from "react";
-import styles from './staticHeader.module.scss';
-import MenuIcon from '@mui/icons-material/Menu';
+import styles from "./staticHeader.module.scss";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 
 const StaticHeader = () => {
   return (
     <>
       <header className={styles.header}>
-        <section className={styles.logo} >
-          <Link href='/'>
-          <section>Powered by</section>
-          <section><sup>TM</sup></section>
-          <section><img src='/images/logo/OISCLogo150.png' /></section>
+        <section className={styles.logo}>
+          <Link href="/">
+            <section>Powered by</section>
+            <section>
+              <sup>TM</sup>
+            </section>
+            <section>
+              <img src="/images/logo/OISCLogo150.png" />
+            </section>
           </Link>
         </section>
-        <section className={styles['burger-menu']}>
+        <section className={styles["burger-menu"]}>
           <MenuIcon />
         </section>
       </header>
