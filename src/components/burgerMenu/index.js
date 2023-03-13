6afe8110e170a5c3d948d936949380c189fb0269
f1vlad <kulchitski@gmail.com>
2023-03-13 12:03:12 -0400

    Responsive burger menu

--- src/components/burgerMenu/index.js
+++ src/components/burgerMenu/index.js
@@ -9,12 +9,22 @@ const BurgerMenu = () => {
     <>
       <nav className={styles.nav}>
         <section className={styles.links}>
-          <Link to="/about">About</Link>
+          <Link to="/about">About Us</Link>
           <Link to="/privacy">Privacy</Link>
           <Link to="/transparency">Transparency</Link>
           <Link to="/careers">Careers</Link>
           <Link to="/feedback">Feedback</Link>
           <Link to="/contact-us">Contact Us</Link>
+          <span className={styles.logo}>
+            <span>We're building</span>
+            <img src="/images/logo/OISCLogo150.png" />
+            <span><sup>TM</sup></span>
+          </span>
+          <Link to="#">Search Index</Link>
+          <Link to="#">News Index</Link>
+          <Link to="#">Video Index</Link>
+          <Link to="#">Encyclodex</Link>
+          <Link to="#">More</Link>
         </section>
       </nav>
     </>
